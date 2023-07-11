2f514cd5f781a981b44c78bc849fd4725c053470
mBriannys <briannysmonserrate@gmail.com>
2023-07-11 16:40:13 -0500

    fixes migration 'user_terms_of_services' for the syntax was wrong

--- db/migrate/20230711212319_create_user_terms_of_services.rb
+++ db/migrate/20230711212319_create_user_terms_of_services.rb
@@ -2,7 +2,7 @@ class CreateUserTermsOfServices < ActiveRecord::Migration[6.0]
   def change
     create_table :user_terms_of_services do |t|
       t.references :user, null: false, index: true, foreign_key: true
-      t.references :term_of_service, null: false, index: true, foreign_key: true
+      t.references :terms_of_service, null: false, index: true, foreign_key: true
       t.timestamp :accept_at
       t.string :ip_address, limit: 60
 
