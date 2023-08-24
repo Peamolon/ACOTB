808d6c55218586fb8ff6ef83dc4bc1bd127a836b
Sergio Peña <sergioeduardo2000@gmail.com>
2023-08-24 15:39:05 -0500

    Add double point to Destroy UserService

--- app/controllers/api/v1/user_profiles_controller.rb
+++ app/controllers/api/v1/user_profiles_controller.rb
@@ -45,7 +45,7 @@ module Api
         if user_ids.present?
           ActiveRecord::Base.transaction do
             user_ids.each do |user_id|
-              destroy_user_service = Users::DestroyUserService.new(id: user_id)
+              destroy_user_service = ::Users::DestroyUserService.new(id: user_id)
               destroy_user_service.call
             end
             render json: { message: 'Users were successfully destroyed' }, status: 200
