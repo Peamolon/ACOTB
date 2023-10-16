611dae0aee86ce45785619d57aa3bbd6c429c752
Sergio Peña <sergioeduardo2000@gmail.com>
2023-10-16 18:40:51 -0500

    create csv controller

--- app/controllers/api/v1/csv_controller.rb
+++ app/controllers/api/v1/csv_controller.rb
@@ -5,21 +5,50 @@ module Api
     class CsvController < ApplicationController
 
       def create_rotations
-        if params[:csvFile].present?
-          file = params[:csvFile].tempfile
-          csv_options = { col_sep: ';' }
+        uploaded_file = params[:csvFile]
+        if uploaded_file
+          csv_data = uploaded_file.read
 
-          puts file
+          parsed_csv = CSV.parse(csv_data, col_sep: ';', headers: true)
 
-          CSV.foreach(file, csv_options) do |row|
-            puts row
+          if parsed_csv
+            parsed_csv.each do |row|
+              name = row['name']
+              start_date = row['start_date']
+              end_date = row['end_date']
+              manager_email = row['manager_email']
+              institution_name = row['institution']
+
+              manager = Manager.joins(user_profile: :user).find_by("users.email = ?", manager_email)
+              institution = Institution.find_by("name = ?", institution_name)
+
+              if manager.nil?
+                raise StandardError.new("Manager not found for email: #{row['manager_email']}")
+              end
+
+              if institution.nil?
+                raise StandardError.new("Institution not found for name: #{row['institution']}")
+              end
+
+              rotation = Rotation.create(
+                name: name,
+                start_date: start_date,
+                end_date: end_date,
+                manager: manager,
+                institution: institution
+              )
+            end
+
+            render json: { message: 'Archivo CSV recibido y procesado exitosamente' }, status: :ok
+          else
+            render json: { error: 'No se pudo analizar el archivo CSV' }, status: :bad_request
           end
-
+        else
+          render json: { error: 'No se proporcionó un archivo CSV' }, status: :unprocessable_entity
         end
+      rescue StandardError => e
+        render json: { error: e.message }, status: :unprocessable_entity
       end
-
     end
   end
 end
-
-
